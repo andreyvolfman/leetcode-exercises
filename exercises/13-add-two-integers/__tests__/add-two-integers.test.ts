8c92df9f2580bc999bb0578ccd271a060c46c79d
Andrey <andrey@iMac.local>
2022-05-31 20:04:37 +0300

    add-two-integers

---
+++ exercises/13-add-two-integers/__tests__/add-two-integers.test.ts
@@ -0,0 +1,25 @@
+import { sum } from '../add-two-integers';
+
+describe('sum', () => {
+    test('', () => {
+        const num1 = 12;
+        const num2 = 5;
+
+        const output = sum(num1, num2);
+
+        const expected = 17;
+
+        expect(output).toEqual(expected);
+    });
+
+    test('', () => {
+        const num1 = -10;
+        const num2 = 4;
+
+        const output = sum(num1, num2);
+
+        const expected = -6;
+
+        expect(output).toEqual(expected);
+    });
+});
