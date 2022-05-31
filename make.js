0116431a41b4f3c8e2cad7c2a1abea5e28f8609a
Andrey <andrey@iMac.local>
2022-05-31 20:16:28 +0300

    add make script

---
+++ make.js
@@ -0,0 +1,32 @@
+import { promises, open } from 'fs';
+import path from 'path';
+import { exec } from 'child_process';
+
+const [, , challengeName] = process.argv;
+
+exec(`git checkout master && git pull && git checkout -b ${challengeName}`);
+
+function getNextIndex() {
+    return promises
+        .readdir('./exercises')
+        .then((files) => files.map((file) => Number(file.split('-')[0])))
+        .then((indices) => Math.max(...indices) + 1);
+}
+
+const nextIndex = await getNextIndex();
+
+const newChallenge = path.resolve(
+    './exercises',
+    `${nextIndex}-${challengeName}`
+);
+
+await promises.mkdir(newChallenge);
+await promises.mkdir(path.resolve(newChallenge, '__tests__'));
+
+await Promise.all([
+    promises.open(path.resolve(newChallenge, `${challengeName}.ts`), 'w'),
+    promises.open(
+        path.resolve(newChallenge, '__tests__', `${challengeName}.test.ts`),
+        'w'
+    ),
+]);
